ba26f5a3a02903505303a28deab7f449adcd7986
Yusuf Khan <53833793+Yusuf4all@users.noreply.github.com>
2020-01-27 17:27:48 +0530

    Add files via upload

---
+++ precache-manifest.96ed0acfa8dc4b0b0d63bf0f10bc74df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f11755dd718bf665e29742ce30791dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4dfca7f7d3d3e105c0c",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "30cabff77c8ac0d1dcf6",
+    "url": "/static/css/main.3113dddd.chunk.css"
+  },
+  {
+    "revision": "e4dfca7f7d3d3e105c0c",
+    "url": "/static/js/2.fc3375f6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.fc3375f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30cabff77c8ac0d1dcf6",
+    "url": "/static/js/main.e57017e1.chunk.js"
+  },
+  {
+    "revision": "10eaa634e3c115aead3f",
+    "url": "/static/js/runtime-main.51b7d066.js"
+  },
+  {
+    "revision": "629ea48a7f70fd33e8635356795fb20a",
+    "url": "/static/media/spin.629ea48a.gif"
+  }
+]);
